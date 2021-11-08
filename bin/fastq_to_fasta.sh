2287fbcb7a6aef16998856c53d4882d04bed2e3f
Chela James <chela.james@icr.ac.uk>
2021-11-08 16:04:04 +0000

    fixed fasq to fasta

--- bin/fastq_to_fasta.sh
+++ bin/fastq_to_fasta.sh
@@ -2,4 +2,5 @@
 infile=$1
 outfile=$2
 
-zcat $infile | sed -n '1~4s/^@/>/p;2~4p' > $outfile
+zcat $infile | sed -n '1~4s/^@/>/p;2~4p' |
+sed 's/ /:/g' | awk 'BEGIN{OFS=""}{if(NR%2==0){print $0}else{print $0,"/0/","0_",length}}' > $outfile
